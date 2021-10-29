79203850e5f8b22d26e2712bb01a7c655f51b51a
SaekiTominaga <kcsu3470+github@gmail.com>
2021-10-29 21:28:16 +0900

    fix #1 Content-Type の設定を express.static 内に移行、不要な debug ログ出力削除

--- node/src/app.ts
+++ node/src/app.ts
@@ -16,48 +16,15 @@ const logger = Log4js.getLogger();
 
 const app = Express();
 
+const EXTENTIONS: { readonly [s: string]: string } = {
+	map: '.map',
+}; // 静的ファイル拡張子の定義
+
 app.set('x-powered-by', false);
 app.set('trust proxy', true);
 app.set('views', config.views);
 app.set('view engine', 'ejs');
-app.use((req, res, next) => {
-	const requestPath = req.path;
-
-	let requestFilePath: string | undefined; // 実ファイルパス
-	if (requestPath.endsWith('/')) {
-		/* ディレクトリトップ（e.g. /foo/ ） */
-		const fileName = config.static.indexes?.find((name) => fs.existsSync(`${config.static.root}/${requestPath}${name}`));
-		if (fileName !== undefined) {
-			requestFilePath = `${requestPath}${fileName}`;
-		}
-	} else if (path.extname(requestPath) === '') {
-		/* 拡張子のない URL（e.g. /foo ） */
-		const extension = config.static.extensions?.find((ext) => fs.existsSync(`${config.static.root}/${requestPath}.${ext}`));
-		if (extension !== undefined) {
-			requestFilePath = `${requestPath}.${extension}`;
-		}
-	} else {
-		/* 拡張子のある URL（e.g. /foo.txt ） */
-		requestFilePath = requestPath;
-	}
-
-	/* Content-Type */
-	const mimeOfPath = Object.entries(config.static.headers.mime.path).find(([, paths]) => requestFilePath !== undefined && paths.includes(requestFilePath))?.[0]; // ファイルパスから決定される MIME
-	const mimeOfExtension = Object.entries(config.static.headers.mime.extension).find(
-		([, extensions]) => requestFilePath !== undefined && extensions.includes(path.extname(requestFilePath).substring(1))
-	)?.[0]; // 拡張子から決定される MIME
-	const mime = mimeOfPath ?? mimeOfExtension;
-
-	if (mime === undefined) {
-		if (requestFilePath !== undefined) {
-			logger.info('MIME が未定義のファイル', requestPath);
-		}
-	} else {
-		logger.debug('Content-Type', `${requestPath} - ${mime}`);
-
-		res.setHeader('Content-Type', mime);
-	}
-
+app.use((_req, res, next) => {
 	/* HSTS */
 	res.setHeader('Strict-Transport-Security', config.response.header.hsts);
 
@@ -75,17 +42,26 @@ app.use(
 		extensions: config.static.extensions,
 		index: config.static.indexes,
 		setHeaders: (res, localPath) => {
-			const requestUrl = res.req.url;
-			const extension = path.extname(localPath); // 拡張子
+			const requestUrl = res.req.url; // リクエストパス e.g. ('/foo.html.br')
+			const requestUrlOrigin = requestUrl.endsWith(EXTENTIONS.brotli) ? requestUrl.substring(0, requestUrl.length - EXTENTIONS.brotli.length) : requestUrl; // 元ファイル（圧縮ファイルではない）のリクエストパス (e.g. '/foo.html')
+			const localPathOrigin = localPath.endsWith(EXTENTIONS.brotli) ? localPath.substring(0, localPath.length - EXTENTIONS.brotli.length) : localPath; // 元ファイルの絶対パス (e.g. '/var/www/public/foo.html')
+			const extensionOrigin = path.extname(localPathOrigin); // 元ファイルの拡張子 (e.g. '.html')
 
-			/* Cache */
+			/* Content-Type */
+			const mime =
+				Object.entries(config.static.headers.mime.path).find(([, paths]) => paths.includes(requestUrlOrigin))?.[0] ??
+				Object.entries(config.static.headers.mime.extension).find(([, extensions]) => extensions.includes(extensionOrigin.substring(1)))?.[0];
+			if (mime === undefined) {
+				logger.error('MIME が未定義のファイル', requestUrlOrigin);
+			}
+			res.setHeader('Content-Type', mime ?? 'application/octet-stream');
+
+			/* Cache-Control */
 			if (config.static.headers.cache_control !== undefined) {
 				const cacheControlValue =
-					config.static.headers.cache_control.path.find((path) => path.paths.includes(requestUrl))?.value ??
-					config.static.headers.cache_control.extension.find((ext) => ext.extensions.includes(extension))?.value ??
+					config.static.headers.cache_control.path.find((path) => path.paths.includes(requestUrlOrigin))?.value ??
+					config.static.headers.cache_control.extension.find((ext) => ext.extensions.includes(extensionOrigin))?.value ??
 					config.static.headers.cache_control.default;
-
-				logger.debug('Cache-Control', `${requestUrl} - ${cacheControlValue}`);
 
 				res.setHeader('Cache-Control', cacheControlValue);
 			}
