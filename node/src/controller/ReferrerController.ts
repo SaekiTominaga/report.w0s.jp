c53ee63ec84f4b1e46100493d58b06a23ef72876
SaekiTominaga <kcsu3470+github@gmail.com>
2024-12-13 19:05:57 +0900

    データ重複チェックを実装（Referrer）

--- node/src/controller/ReferrerController.ts
+++ node/src/controller/ReferrerController.ts
@@ -20,26 +20,35 @@ export default class ReferrerController extends Controller implements Controller
 		const ua = req.header('User-Agent');
 		const ipAddress = ip.address();
 
-		/* エラー内容をDBに記録 */
 		const dbFilePath = process.env['SQLITE_REPORT'];
 		if (dbFilePath === undefined) {
 			throw new HTTPException(500, { message: 'DB file path not defined' });
 		}
 
 		const dao = new ReportReferrerDao(dbFilePath);
-		await dao.insert({
-			location: location,
+
+		const existSameData = await dao.same({
+			pageUrl: location,
 			referrer: referrer,
 		});
 
-		/* エラー内容を通知 */
-		const html = await ejs.renderFile(`${process.env['VIEWS'] ?? ''}/referrer_mail.ejs`, {
-			location: location,
-			referrer: referrer,
-			ua: ua,
-			ip: ipAddress,
-		});
-		await new Mail().sendHtml(process.env['REFERRER_MAIL_TITLE'], html);
+		if (!existSameData) {
+			/* DB に登録 */
+			await dao.insert({
+				pageUrl: location,
+				referrer: referrer,
+			});
+
+			/* メール通知 */
+			const html = await ejs.renderFile(`${process.env['VIEWS'] ?? ''}/referrer_mail.ejs`, {
+				location: location,
+				referrer: referrer,
+				ua: ua,
+				ip: ipAddress,
+			});
+
+			await new Mail().sendHtml(process.env['REFERRER_MAIL_TITLE'], html);
+		}
 
 		return new Response(null, {
 			status: 204,
