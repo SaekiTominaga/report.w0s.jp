e6676daef273eda727b1f134802ba011f04929a2
SaekiTominaga <kcsu3470+github@gmail.com>
2025-01-16 23:00:12 +0900

    Reporting API v1 に対応

--- node/src/validator/csp.ts
+++ node/src/validator/csp.ts
@@ -2,13 +2,13 @@ import { HTTPException } from 'hono/http-exception';
 import { validator } from 'hono/validator';
 
 interface RequestHeader {
-	contentType: 'application/csp-report';
+	contentType: 'application/reports+json' | 'application/csp-report';
 }
 
 export const header = validator('header', (value): RequestHeader => {
 	const { 'content-type': contentType } = value;
 
-	if (contentType !== 'application/csp-report') {
+	if (contentType !== 'application/reports+json' && contentType !== 'application/csp-report') {
 		throw new HTTPException(400, { message: 'The `Content-Type` request header is invalid' });
 	}
 
