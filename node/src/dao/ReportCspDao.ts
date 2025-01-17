970fa347814285e2071480be23459de7f45eb979
SaekiTominaga <kcsu3470+github@gmail.com>
2025-01-17 12:11:32 +0900

    Reporting API v1 が配列形式になっていなかったバグを修正

--- node/src/dao/ReportCspDao.ts
+++ node/src/dao/ReportCspDao.ts
@@ -58,37 +58,45 @@ export default class ReportJsDao extends ReportDao {
 	/**
 	 * エラー内容を DB に登録
 	 *
-	 * @param data - 登録データ
+	 * @param datas - 登録データ
 	 */
-	async insert(data: Readonly<Omit<ReportDB.CSP, 'registeredAt'>>): Promise<void> {
+	async insert(datas: Readonly<Omit<ReportDB.CSP, 'registeredAt'>>[]): Promise<void> {
+		if (datas.length === 0) {
+			return;
+		}
+
 		const dbh = await this.getDbh();
 
 		await dbh.exec('BEGIN');
 		try {
-			const sth = await dbh.prepare(`
-				INSERT INTO
-					d_csp
-					( document_url,  referrer,  blocked_url,  effective_directive,  original_policy,  source_file,  sample,  disposition,  status_code,  line_number,  column_number,  ua,  ip,  registered_at)
-				VALUES
-					(:document_url, :referrer, :blocked_url, :effective_directive, :original_policy, :source_file, :sample, :disposition, :status_code, :line_number, :column_number, :ua, :ip, :registered_at)
-			`);
-			await sth.run({
-				':document_url': data.documentURL,
-				':referrer': data.referrer,
-				':blocked_url': data.blockedURL,
-				':effective_directive': data.effectiveDirective,
-				':original_policy': data.originalPolicy,
-				':disposition': data.disposition,
-				':status_code': data.statusCode,
-				':sample': data.sample ?? null,
-				':source_file': data.sourceFile ?? null,
-				':line_number': data.lineNumber ?? null,
-				':column_number': data.columnNumber ?? null,
-				':ua': data.ua ?? null,
-				':ip': data.ip,
-				':registered_at': Math.round(Date.now() / 1000),
-			});
-			await sth.finalize();
+			await Promise.all(
+				datas.map(async (data) => {
+					const sth = await dbh.prepare(`
+						INSERT INTO
+							d_csp
+							( document_url,  referrer,  blocked_url,  effective_directive,  original_policy,  source_file,  sample,  disposition,  status_code,  line_number,  column_number,  ua,  ip,  registered_at)
+						VALUES
+							(:document_url, :referrer, :blocked_url, :effective_directive, :original_policy, :source_file, :sample, :disposition, :status_code, :line_number, :column_number, :ua, :ip, :registered_at)
+					`);
+					await sth.run({
+						':document_url': data.documentURL,
+						':referrer': data.referrer,
+						':blocked_url': data.blockedURL,
+						':effective_directive': data.effectiveDirective,
+						':original_policy': data.originalPolicy,
+						':disposition': data.disposition,
+						':status_code': data.statusCode,
+						':sample': data.sample ?? null,
+						':source_file': data.sourceFile ?? null,
+						':line_number': data.lineNumber ?? null,
+						':column_number': data.columnNumber ?? null,
+						':ua': data.ua ?? null,
+						':ip': data.ip,
+						':registered_at': Math.round(Date.now() / 1000),
+					});
+					await sth.finalize();
+				}),
+			);
 
 			await dbh.exec('COMMIT');
 		} catch (e) {
