bfaee1205f6e46d4fa00e940be1e3f3f576a9a91
SaekiTominaga <kcsu3470+github@gmail.com>
2025-09-20 16:55:36 +0900

    フィルターデータ追加

--- node/src/config/csp.ts
+++ node/src/config/csp.ts
@@ -8,12 +8,14 @@ interface Filter {
 const noticeFilter: Filter[] = [
 	{ effectiveDirective: 'connect-src', blockedURL: /^https:\/\/analytics\.w0s\.jp\/matomo\/matomo\.php\?/v },
 	{ effectiveDirective: 'font-src', blockedURL: /^https:\/\/fonts\.gstatic\.com\/s\//v },
+	{ effectiveDirective: 'img-src', blockedURL: 'blob', sourceFile: /^https:\/\/w0s\.jp:443\//v }, // iPad
 	{ effectiveDirective: 'media-src', blockedURL: 'data' }, // moz-extension (NoScript)
 	{ effectiveDirective: 'script-src', blockedURL: 'https://analytics.w0s.jp' }, // old Safari
 	{ effectiveDirective: 'script-src', blockedURL: 'https://analytics.w0s.jp/matomo/matomo.js' }, // old Safari
 	{ effectiveDirective: 'script-src', blockedURL: 'eval', sourceFile: 'chrome-extension' },
 	{ effectiveDirective: 'script-src', blockedURL: 'wasm-eval', sourceFile: 'chrome-extension' },
 	{ effectiveDirective: 'script-src-elem', blockedURL: 'inline', sourceFile: 'moz-extension' }, // moz-extension (Violentmonkey)
+	{ effectiveDirective: 'script-src-elem', blockedURL: 'inline', sourceFile: 'user-script' }, // iPad
 	{
 		effectiveDirective: 'script-src-elem',
 		blockedURL: 'inline',
@@ -25,8 +27,10 @@ const noticeFilter: Filter[] = [
 		sourceFile: /^safari-web-extension:\/\/[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\/js\/utils\.js$/v,
 	},
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'default' },
+	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'default2' },
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'dompurify' },
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'sanitizer' },
+	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'vue' },
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: '@azure/ms-rest-js#xml.browser' },
 	{ effectiveDirective: 'require-trusted-types-for', blockedURL: 'trusted-types-sink', sample: 'Function|(\n) {\n\n})' }, // old Safari
 	{ effectiveDirective: 'fenced-frame-src' },
