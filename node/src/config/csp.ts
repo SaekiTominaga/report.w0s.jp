9baf31e29ff213a76b1590b4279de5691e084783
SaekiTominaga <kcsu3470+github@gmail.com>
2025-09-21 17:19:08 +0900

    フィルターデータ追加

--- node/src/config/csp.ts
+++ node/src/config/csp.ts
@@ -32,6 +32,7 @@ const noticeFilter: Filter[] = [
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'sanitizer' },
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: 'vue' },
 	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'chrome-extension', sample: '@azure/ms-rest-js#xml.browser' },
+	{ effectiveDirective: 'trusted-types', blockedURL: 'trusted-types-policy', sourceFile: 'user-script', sample: 'dompurify' }, // iPad
 	{ effectiveDirective: 'require-trusted-types-for', blockedURL: 'trusted-types-sink', sample: 'Function|(\n) {\n\n})' }, // old Safari
 	{ effectiveDirective: 'fenced-frame-src' },
 ];
